2efd54bdfcd4ca8dc8f45aa9e8bf494a7d30e59c
Christiaan Roeleveld <croeleveld@gmail.com>
2015-01-05 18:40:20 +0100

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'ITQ Demo', server: os.hostname() });
+  res.render('index', { title: 'DemoApp', server: os.hostname() });
 });
 
 module.exports = router;
